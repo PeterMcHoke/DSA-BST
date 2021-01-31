fbb083a9ec446cb8120405216e67294712461e1d
PeterMcHoke <petermchoke@gmail.com>
2021-01-31 15:52:55 -0600

    First commit

---
+++ BST.js
@@ -0,0 +1,9 @@
+class BST {
+    constructor(key=null, value=null, parent=null) {
+        this.key = key;
+        this.value = value;
+        this.parent = parent;
+        this.left = null;
+        this.right = null;
+    }
+}
